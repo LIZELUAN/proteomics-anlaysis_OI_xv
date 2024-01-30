8b0a71b212482ed89949c24a0ce405bba42933b8
LIZELUAN <104906671+LIZELUAN@users.noreply.github.com>
2024-01-30 15:02:04 +0800

    Add files via upload

---
+++ Step4.2 data_imputation & PCA & SVM (DEG).R
@@ -0,0 +1,148 @@
+setwd("C:/Users/may/Desktop/project/diann-4vs4-20230529")
+library(tidyverse)
+library(DEP)
+library(openxlsx)
+# BiocManager::install("DEP")
+# colnames(da)
+da <- read_csv("txt/report.pg_matrix.csv") %>%
+  mutate(across(starts_with("Control")|starts_with("OI"), ~log2(.x))) %>%
+  select(c(-Control1,-OI1))
+da$`Genes` %>% duplicated() %>% any()
+
+## if TRUE  :) #######################
+# Find the duplicated genes
+da %>% group_by(`Genes`) %>% summarise(frequency = n()) %>%
+  arrange(desc(frequency)) %>% filter(frequency > 1)
+
+# 确保基因名唯一
+# 使用Gene.names中的注释作为主要名称,使用Protein.IDs中的注释作为没有基因名的蛋白质的ID
+da <- make_unique(da,"Genes","Protein.Ids",delim = ";")
+
+# 验证
+da$name %>% duplicated() %>% any()
+# [1] FALSE
+
+da <- da %>% select(name,starts_with("Control"),starts_with("OI")) %>%
+  rename(Gene=name)
+
+# =========== NA statistics ====================
+lfq1 <- da %>%
+  select(-Gene)
+val1 <- sum(!is.na(lfq1))
+na1 <- sum(is.na(lfq1))
+
+# =====================过滤缺失值=====================
+da <- da %>%
+  rowwise() %>%
+  mutate(Num_valid_Ctrl = sum(!is.na(c_across(starts_with("Control"))))) %>%
+  mutate(Num_valid_OI = sum(!is.na(c_across(starts_with("OI"))))) %>% ungroup() %>%
+  filter(!(Num_valid_Ctrl+Num_valid_OI==0))
+# Select protein subset that capture the detected values#
+
+# ========== Data Winsorisation ==========
+da <- da %>% 
+  rowwise() %>% 
+  mutate(mean_exp=mean(c_across(starts_with("Control") | starts_with("OI")),na.rm=T)) %>% ungroup()
+
+cut_point_top <- quantile(da$mean_exp, 0.95)
+cut_point_bottom <- quantile(da$mean_exp, 0.05)
+
+da_filtered <- da %>% 
+  filter(mean_exp < cut_point_top & mean_exp > cut_point_bottom) %>%
+  filter(Num_valid_Ctrl>=2 & Num_valid_OI>=2) # # Select protein subset that capture the detected values#
+
+
+
+# =========== NA statistics ====================
+lfq2 <- da_filtered %>%
+  select(starts_with("Control") | starts_with("OI"))
+val2 <- sum(!is.na(lfq2))
+na2 <- sum(is.na(lfq2))
+valid_percent <- val2/val1*100
+na_percent <- na2/na1*100
+
+
+
+
+
+# =========== Da imputation by MICE ====================
+library(mice)
+lfq <- da_filtered %>%
+  select(starts_with("Control") | starts_with("OI"))
+
+imputed <- mice(lfq, m=5, maxit=50, meth = "pmm", seed=500)  ####### 生成五个数据集 
+lfq <- complete(imputed,1)
+# 更新数据
+da_filtered[,colnames(lfq)]<-lfq
+
+
+
+
+# =========Data scaling & PCA ============
+data_normalized <- da_filtered %>%
+  filter(Gene %in% DEG$Gene) %>%
+  select(starts_with("Control") | starts_with("OI"))
+# da[,colnames(data_normalized)]<-data_normalized
+# 
+
+# ============ PCA analysis ================
+##################### Draw PCA based on DEP(statistical) ######################
+pca <- prcomp(t(data_normalized))
+# plot(pca$x, main="after PCA")
+pca.var <- pca$sdev^2
+pca.var.per <- round(pca.var/sum(pca.var)*100, 1)
+
+library(ggplot2)
+library(ggrepel)
+pca.data <- data.frame(Sample=rownames(pca$x),
+                       X=pca$x[,1],
+                       Y=pca$x[,2],
+                       Group=c("Control","Control","Control","OI","OI","OI")) 
+
+pdf("DEG results/PCA.pdf",w=3,h=3)
+ggplot(data=pca.data, aes(x=X, y=Y)) +
+  geom_point(aes(color = Group),size=3) +
+  geom_text_repel(aes(label=Sample, color=Group), show.legend = F) +
+  scale_color_manual(values = c("orange","purple")) +
+  xlab(paste("PC1 - ", pca.var.per[1], "%", sep = "")) +
+  ylab(paste("PC2 - ", pca.var.per[2], "%", sep = "")) +
+  theme_classic() + theme(legend.position="None") +
+  ggtitle("PCA graph")  
+  #stat_ellipse(aes(fill = Group), level = 0.95, geom = 'polygon', alpha=0.1, show.legend = FALSE) +
+  #scale_fill_manual(values = c('orange', 'purple'))
+
+dev.off()
+
+
+# SVM
+library(e1071)
+library(grid)
+library(lattice)
+library(gridExtra)
+library(stringr)
+
+# Read in the data
+dat <- data.frame(pca$x[,c(1,2)],Group=c(-1,-1,-1,1,1,1))
+# Create a logistic rgression model on the reduced data
+svmfit = svm(Group~.,data=dat,kernel="polynomial",coef0=1)
+
+# plot(svmfit,dat)
+# Visualize the decision boundaries for training set
+X1 = seq(range(pca$x[,1])[1]*2, range(pca$x[,1])[2], len=20)
+X2 = seq(range(pca$x[,2])[1]*1.5, range(pca$x[,2])[2]*1.05, len=20)
+xgrid = expand.grid(PC1=X1, PC2=X2)
+ygrid = predict(svmfit, xgrid)
+
+# ygrid <- predict(svmfit, xgrid, decision.values=T)
+pdf("DEG results/PCA_SVM_polynomial.pdf",w=4,h=4)
+plot(dat[,-3], cex=2,col= alpha(ifelse(dat$Group == -1, "lightblue","red"),0.5),pch = 19,
+     main = 'SVM after PCA',
+     xlab(paste("PC1 - ", pca.var.per[1], "%", sep = "")) ,
+       ylab(paste("PC2 - ", pca.var.per[2], "%", sep = "")))
+contour(X1, X2, matrix(as.numeric(ygrid),20,20), add=TRUE,level=0)
+contour(X1, X2, matrix(as.numeric(ygrid), 20,20), add=TRUE,level=0.5,lwd=2,lty=2,col="red")
+contour(X1, X2, matrix(as.numeric(ygrid), 20,20), add=TRUE,level=-0.5,lwd=2,lty=2,col="blue")
+text(dat[,-3],labels=row.names(dat),cex=0.3)
+dev.off()
+
+
